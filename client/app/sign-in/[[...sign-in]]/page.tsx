511fb77ea02f89bffe5e336f4c1a041669afad36
marcadrian-it <marcadrian.it@gmail.com>
2023-11-28 02:16:20 +0100

    (opt): model optimisations

--- client/app/sign-in/[[...sign-in]]/page.tsx
+++ client/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
-import { SignIn } from '@clerk/nextjs';
+import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
-  return <SignIn />;
+  return (
+    <div className="bg-[#ddd1f4] flex w-screen h-screen flex-col items-center justify-center">
+      <SignIn />
+    </div>
+  );
 }
--- client/app/sign-up/[[...sign-up]]/page.tsx
+++ client/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
-import { SignUp } from '@clerk/nextjs';
+import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-  return <SignUp afterSignUpUrl="/new-user" redirectUrl="/new-user" />;
+  return (
+    <div className="bg-[#ddd1f4] flex w-screen h-screen flex-col items-center justify-center">
+      <SignUp afterSignUpUrl="/new-user" redirectUrl="/new-user" />
+    </div>
+  );
 }
